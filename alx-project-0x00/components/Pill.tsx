c6fc5974bea58be339a7a0d1ede6b09647957e32
Otega Otite <otegaotite@gmail.com>
2025-09-30 11:30:57 +0100

    Rendering components

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,9 +1,9 @@
 
 const Pill: React.FC = () => {
     return (
-        <div>
-            Pill Components
-        </div>
+        <div className="flex justify-center items-center bg-[#f9f9f9] px-3 w-auto h-[27px] rounded-full">
+            <p className="text-sm">Title</p>
+        </div >
     )
 }
 export default Pill;
